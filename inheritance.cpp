cd08e6d79cd4afed3b25ce91f3cc4ec9e9812201
MissZ <zhuangwang9793@163.com>
2025-12-07 16:31:17 +0800

    first project

---
+++ inheritance.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+
+using namespace std;
+
+class Base1 {
+public:
+    Base1() {
+        cout << "Base1 construction" << endl;
+    }
+    ~Base1() {
+        cout << "Base1 destruction" << endl;
+    }
+};
+
+class Base {
+public:
+    Base() {
+        cout << "Base construction" << endl;
+    }
+    ~Base() {
+        cout << "Base destruction" << endl;
+    }
+
+private:
+    // 添加成员变量
+    Base1 b1;
+};
+
+class Drive1 {
+public:
+    Drive1() {
+        cout << "Drive1 construction" << endl;
+    }
+    ~Drive1() {
+        cout << "Drive1 destruction" << endl;
+    }
+};
+
+class Drive : public Base{
+public:
+    Drive() {
+        cout << "Drive construction" << endl;
+    }
+    ~Drive() {
+        cout << "Drive destruction" << endl;
+    }
+private:
+    Drive1 d1;
+};
+
+
+// ==================================
+class MBase1 {
+public:
+    MBase1() {
+        cout << "MBase1 construction" << endl;
+    }
+    ~MBase1() {
+    
+        cout << "MBase1 destruction" << endl;
+    }
+};
+
+class MBase2 {
+public:
+    MBase2() {
+        cout << "MBase2 construction" << endl;
+    }
+    ~MBase2() {
+            cout << "MBase2 destruction" << endl;
+    }
+};
+
+class MBase1 {
+public:
+    MBase1() {
+        cout << "MBase1 construction" << endl;
+    }
+    ~MBase1() {
+    
+        cout << "MBase1 destruction" << endl;
+    }
+};
+
+class MDrive : public MBase1, public MBase2{    // MDrive依赖于MBase1和MBase2
+public:
+    MDrive() {
+        cout << "MDrive construction" << endl;
+     }
+    ~MDrive() {
+    
+        cout << "MDrive destruction" << endl;
+    }
+};
+
+int main(){
+    Drive d;
+    return 0;
+}
