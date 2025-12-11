3fb4a482faa7b6bcd2fa44e7273448f79d37b322
Lili <zhuangwang9793@163.com>
2025-12-11 19:46:25 +0800

    addformate

--- inheritance.cpp
+++ inheritance.cpp
@@ -3,113 +3,73 @@
 using namespace std;
 
 class Base1 {
-public:
-    Base1() {
-        cout << "Base1 construction" << endl;
-    }
-    ~Base1() {
-        cout << "Base1 destruction" << endl;
-    }
+  public:
+    Base1() { cout << "Base1 construction" << endl; }
+    ~Base1() { cout << "Base1 destruction" << endl; }
 };
 
 class Base {
-public:
-    Base() {
-        cout << "Base construction" << endl;
-    }
-    ~Base() {
-        cout << "Base destruction" << endl;
-    }
+  public:
+    Base() { cout << "Base construction" << endl; }
+    ~Base() { cout << "Base destruction" << endl; }
 
-private:
+  private:
     // 添加成员变量
     Base1 b1;
 };
 
 class Drive1 {
-public:
-    Drive1() {
-        cout << "Drive1 construction" << endl;
-    }
-    ~Drive1() {
-        cout << "Drive1 destruction" << endl;
-    }
+  public:
+    Drive1() { cout << "Drive1 construction" << endl; }
+    ~Drive1() { cout << "Drive1 destruction" << endl; }
 };
 
-class Drive : public Base{  // Drive依赖于Base,这里的Base是基类
-public:
-    Drive() {
-        cout << "Drive construction" << endl;
-    }
-    ~Drive() {
-        cout << "Drive destruction" << endl;
-    }
-private:
+class Drive : public Base { // Drive依赖于Base,这里的Base是基类
+  public:
+    Drive() { cout << "Drive construction" << endl; }
+    ~Drive() { cout << "Drive destruction" << endl; }
+
+  private:
     Drive1 d1;
 };
 
-
 // ==================================
 class MBase1 {
-public:
-    MBase1() {
-        cout << "MBase1 construction" << endl;
-    }
-    ~MBase1() {
-    
-        cout << "MBase1 destruction" << endl;
-    }
+  public:
+    MBase1() { cout << "MBase1 construction" << endl; }
+    ~MBase1() { cout << "MBase1 destruction" << endl; }
 };
 
 class MBase2 {
-public:
-    MBase2() {
-        cout << "MBase2 construction" << endl;
-    }
-    ~MBase2() {
-            cout << "MBase2 destruction" << endl;
-    }
+  public:
+    MBase2() { cout << "MBase2 construction" << endl; }
+    ~MBase2() { cout << "MBase2 destruction" << endl; }
 };
 
 class MDrive1 {
-public:
-    MDrive1() {
-        cout << "MDrive1 construction" << endl;
-    }
-    ~MDrive1() {
-    
-        cout << "MDrive1 destruction" << endl;
-
-    }
+  public:
+    MDrive1() { cout << "MDrive1 construction" << endl; }
+    ~MDrive1() { cout << "MDrive1 destruction" << endl; }
 };
 
 class MDrive2 {
-public:
-    MDrive2 () {
-        cout << "MDrive2  construction" << endl;
-    }
-    ~MDrive2 () {
-    
-        cout << "MDrive2  destruction" << endl;
-        
-    }
+  public:
+    MDrive2() { cout << "MDrive2  construction" << endl; }
+    ~MDrive2() { cout << "MDrive2  destruction" << endl; }
 };
 
-class MDrive : public MBase1, public MBase2{    // MDrive依赖于MBase1和MBase2
-public:
-    MDrive() {
-        cout << "MDrive construction" << endl;
-     }
-    ~MDrive() {
-    
-        cout << "MDrive destruction" << endl;
-    }
-private:
+class MDrive : public MBase1, public MBase2 { // MDrive依赖于MBase1和MBase2
+  public:
+    MDrive() { cout << "MDrive construction" << endl; }
+    ~MDrive() { cout << "MDrive destruction" << endl; }
+
+  private:
     MDrive1 md1;
     MDrive2 md2;
 };
 
-int main(){
+int main()
+{
     // Drive d;
     MDrive md;
     return 0;
